62f2398d007ac1bf66ba195b35a2762902296826
KevinChiputra <kevin.chiputra@binus.ac.id>
2024-02-20 13:34:12 +0700

    tambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem accusantium quae dolorum repellat atque, incidunt voluptatem quod expedita error corrupti blanditiis maxime porro dignissimos magnam dolor aliquam saepe consequuntur velit?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
