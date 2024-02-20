bcd362ff564d2f9648d44c799e086229e834908e
KevinChiputra <kevin.chiputra@binus.ac.id>
2024-02-20 13:28:37 +0700

    menambahkan tag p

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem accusantium quae dolorum repellat atque, incidunt voluptatem quod expedita error corrupti blanditiis maxime porro dignissimos magnam dolor aliquam saepe consequuntur velit?</p>
 </body>
 </html>
